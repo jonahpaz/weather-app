f82a8e417ed5f1af8657b663211c2bcc0bac833d
Jonah <156857076+jonahpaz@users.noreply.github.com>
2026-04-22 23:28:44 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+//Remember to import local images to use them
+//import odinImage from "./odin.png";
+
+//This is for having dist in another branch.
+// git add dist -f && git commit -m "Deployment commit"
+// git subtree push --prefix dist origin gh-pages
+// git checkout main
